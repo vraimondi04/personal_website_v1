37bf9555ce4b3ca045ab3d0569afbf40a7c8132b
Vincent Raimondi <vincent837869@gmail.com>
2022-08-20 22:47:08 -0500

    Added template within Container component

--- src/front/Components/App.tsx
+++ src/front/Components/App.tsx
@@ -5,11 +5,12 @@ import {
 } from 'react-router-dom'
 
 import '../style.scss'
+import Container from './Container'
 
 export function App () {
     return (<BrowserRouter>
         <Routes>
-            <Route path="/" element={<p>Hello World</p>} />
+            <Route path="/" element={<Container/>} />
         </Routes>
     </BrowserRouter>
     )
---
+++ src/front/Components/Container.tsx
@@ -0,0 +1,76 @@
+
+export default function Container () {
+    // have function that makes axios post request for spotify information from backend
+    return (
+        <section className="hero is-link is-fullheight">
+            {/* Hero head: will stick at the top */}
+            <div className="hero-head">
+                <header className="navbar">
+                    <div className="container">
+                        <div className="navbar-brand">
+                            <a className="navbar-item">
+                                What is Vincent Listening to?
+                            </a>
+                            <span className="navbar-burger" data-target="navbarMenuHeroC">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </div>
+
+                        <div id="navbarMenuHeroC" className="navbar-menu">
+                            <div className="navbar-end">
+                                <a className="navbar-item">
+                                    About
+                                </a>
+                                <a className="navbar-item">
+                                    Projects
+                                </a>
+                                <a className="navbar-item">
+                                    Resume
+                                </a>
+                                <span className="navbar-item">
+                                    <a className="button is-success is-inverted">
+                                        <span className="icon">
+                                            <i className="fab fa-github"></i> {/* change to spotify icon ? */}
+                                        </span>
+                                        <span>Spotify</span>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+
+            {/* Hero content: will be in the middle */}
+            {/* TRY TO IMPLEMENT BULMA TILES INTO THE HERO-BODY */}
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                    <p className="title">
+                        Title
+                    </p>
+                    <p className="subtitle">
+                        Subtitle
+                    </p>
+                </div>
+            </div>
+
+            {/* Hero footer: will stick at the bottom */}
+            <div className="hero-foot">
+                <nav className="tabs is-boxed is-fullwidth">
+                    <div className="container">
+                        <ul>
+                            <li className="is-active"><a>Overview</a></li>
+                            <li><a>Modifiers</a></li>
+                            <li><a>Grid</a></li>
+                            <li><a>Elements</a></li>
+                            <li><a>Components</a></li>
+                            <li><a>Layout</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </section>
+    )
+}
